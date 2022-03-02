90e510b3262ca9343d41023fdd4c51a3e5afecbc
j3ttblack <64046095+j3ttblack@users.noreply.github.com>
2022-03-02 13:05:12 -0700

    Added middle database functionality

--- recipeasy api/server.js
+++ recipeasy api/server.js
@@ -7,6 +7,7 @@ const port = 8008;                              // Port for front-end
 const mysql = require('mysql');
 const { stringify } = require('querystring');
 const { REPL_MODE_SLOPPY } = require('repl');
+const { resourceLimits } = require('worker_threads');
 const person_db = mysql.createConnection({    
     // you need to change all of these to your personal sql login information
     // keep the database the same
@@ -145,8 +146,7 @@ const requestListener = function (req, res) {
                     });
                 } catch(error) { res.end();}
                 break;
-            case "POST":
-                // Create Recipe
+            case "POST":        // Create Recipe
 
                 // convert the request to JSON to read
                 isJSON = false;
@@ -245,8 +245,7 @@ const requestListener = function (req, res) {
                 }
                 res.end();
                 break;
-            case "DELETE":
-                // Delete a Recipe
+            case "DELETE":      // Delete a Recipe
                 if(url_pieces[2]) {
                     r_id = url_pieces[2];
                     try{
@@ -262,8 +261,7 @@ const requestListener = function (req, res) {
                 }
                 res.end();
                 break;
-            case "PUT":
-                // Update Recipe
+            case "PUT":         // Update Recipe
 
                 // convert the request to JSON to read
                 isJSON = false;
@@ -345,6 +343,161 @@ const requestListener = function (req, res) {
                     } else res.end();
                 }
                 res.end();
+                break;
+            default:
+                res.end();
+            }
+        });
+        break;
+    case "middle":
+        req.on('end', () => {
+            let obj;
+            let isJson = false;
+
+            // determine middle functionality by url
+            switch(url_pieces[2]) {
+            case "recipe":
+                // determine method
+                switch(req.method) {
+                case "GET":     // Get Saved Recipes
+                    if(url_pieces[3]) {     // if there is a p_id in the address
+                        try{
+                            // Get all the r_id's corresponding to a p_id
+                            middle_db.query('SELECT r_id FROM Saves WHERE p_id = ' + url_pieces[3], function(err, results){
+                                if(err) throw err;
+                                if(results != null && results.length >0) {
+
+                                    // Convert all the r_ids into a query to get the full recipe
+                                    recipe_query = 'SELECT r.r_id, r.description, r.name, r.likes, r.isVisible, r.date_posted, rc.category, ri.ingredient ' +
+                                    'FROM Recipe as r, Recipe_Categories as rc, Recipe_Ingredients as ri ' + 
+                                    'WHERE r.isVisible = 1 AND r.r_id = rc.r_id AND r.r_id = ri.r_id AND ('
+                                    for(i = 0; i<results.length; i++) {
+                                        if(i!=0) recipe_query += ' OR ';
+                                        recipe_query += 'r.r_id = ' + results[i].r_id;
+                                    }
+                                    recipe_query += ')';
+
+                                    // Get all the recipes of the saved user
+                                    recipe_read_db.query(recipe_query, function(err2, results2) {
+                                        if(err2) throw err;
+                                        if(results2 == null || results2.length == 0) res.write('null', function(err2) {res.end();})
+                                        else {
+                                            /* for recipes, need to also retrieve all of the 
+                                                categories and ingredients. Iterating through to
+                                                combine duplicates and delete other entries
+                                            */
+                                            return_results = [];
+                                            last_r_id = -1;
+                                            for(i = 0; i < results2.length; i++) {
+                                                // find the first instance of the recipe
+                                                if(last_r_id == results2[i].r_id) continue;
+                                                last_r_id = results2[i].r_id;
+
+                                                // create arrays to store duplicates in
+                                                results2[i].categories = [results2[i].category];
+                                                delete results2[i].category;
+                                                results2[i].ingredients = [results2[i].ingredient];
+                                                delete results2[i].ingredient;
+
+                                                // add to return result
+                                                return_results.push(results2[i]);
+
+                                                // iterate through, find duplicate recipes, combine
+                                                for(j = i+1; j < results2.length; j++) {
+                                                    if(results2[i].r_id == results2[j].r_id) {
+                                        
+                                                        //combine categories
+                                                        category_found = false;
+                                                        for(k = 0; k < results2[i].categories.length; k++) {
+                                                            if(results2[i].categories[k] == results2[j].category) category_found = true;
+                                                        }
+                                                        if(!category_found) results2[i].categories.push(results2[j].category);
+
+                                                        //combine ingredients
+                                                        ingredient_found = false;
+                                                        for(k = 0; k < results2[i].ingredients.length; k++) {
+                                                            if(results2[i].ingredients[k] == results2[j].ingredient) ingredient_found = true;
+                                                        }
+                                                        if(!ingredient_found) results2[i].ingredients.push(results2[j].ingredient);
+                                                    }
+                                                }
+                                            }
+
+                                            // return the results, as a string, via http
+                                            res.write(JSON.stringify(return_results), function(err) {res.end();});
+                                        }
+                                    })
+                                } else res.end();
+                            });
+                        } catch(error) { res.end(); }
+                    } else res.end();
+                    break;
+                case "POST":    // Save Recipe
+                    // convert the request to JSON to read
+                    isJSON = false;
+                    try{
+                        obj = JSON.parse(body);
+                        isJSON = true;
+                    } catch(error) {}
+                    if(isJSON) {
+                        // work with the JSON information
+
+                        /* don't want to insert a value into the
+                            sql database with missing information.
+                            The valid boolean will ensure everything
+                            is there. There can be additional attributes,
+                            we don't care about that.
+                        */
+                        let valid = true;
+
+                        // find each attribute we need
+                        let p_id = -1;
+                        if(obj.p_id) p_id = obj.p_id;
+                        else valid = false;
+
+                        let r_id = -1;
+                        if(obj.r_id) r_id = obj.r_id;
+                        else valid = false;
+
+                        
+                        if(valid) {
+                            try{
+                                // Check to see if the entry already exists
+                                middle_db.query('SELECT p_id, r_id FROM Saves WHERE p_id = ' + p_id + ' AND r_id = ' + r_id, function(err, results){
+                                    if(err) throw err;
+                                    if(results == null || results.length == 0) {
+                                        // If there is no entry, create one
+                                        middle_db.query('INSERT INTO Saves (p_id, r_id) VALUES (' + p_id + ',' + r_id + ')');
+                                    }
+                                });
+                            } catch(error) {}
+                        }
+                    }
+                    res.end();
+                    break;
+                default:
+                    res.end();
+                }
+            
+            break;
+            case "user":        // Get Post User
+                if(url_pieces[3] && req.method == 'GET') {     // if there is an r_id in the address and they are requesting a get
+                    try{
+                        middle_db.query('SELECT p_id FROM Posts WHERE r_id = ' + url_pieces[3], function(err, results) {
+                            if(err) throw err;
+                            if(results != null && results.length > 0) {
+                                // only ever one user (unique constraint in table)
+                                person_db.query('SELECT p_id, isVisible, email, password, fname, lname, isAdmin FROM Person WHERE isVisible = 1 AND p_id = ' + results[0].p_id, function(err2, results2) {
+                                    if(err2) throw err2;
+                                    if(results2 != null && results2.length == 1) {
+                                        // return user information
+                                        res.write(JSON.stringify(results2), function(err) {res.end();});
+                                    } else res.end();
+                                });
+                            } else res.end();
+                        }) 
+                    } catch(error) { res.end(); }
+                } else res.end();
                 break;
             default:
                 res.end();
