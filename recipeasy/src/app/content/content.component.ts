08732842c165f630a4ea0424fa4353b2b03ed00a
elistjames <ehstjames@gmail.com>
2022-03-13 14:38:08 -0600

    Added Content page which will be where recipes are received from the api and displayed

---
+++ recipeasy/src/app/content/content.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Card 1', cols: 2, rows: 1 },
+          { title: 'Card 2', cols: 2, rows: 1 },
+          { title: 'Card 3', cols: 2, rows: 1 },
+          { title: 'Card 4', cols: 2, rows: 1 },
+          { title: 'Card 5', cols: 2, rows: 1 },
+          { title: 'Card 6', cols: 2, rows: 1 },
+          { title: 'Card 7', cols: 2, rows: 1 },
+          { title: 'Card 8', cols: 2, rows: 1 }
+        ];
+      }
+
+      return [
+        { title: 'Card 1', cols: 1, rows: 1 },
+        { title: 'Card 2', cols: 1, rows: 1 },
+        { title: 'Card 3', cols: 1, rows: 1 },
+        { title: 'Card 4', cols: 1, rows: 1 },
+        { title: 'Card 5', cols: 1, rows: 1 },
+        { title: 'Card 6', cols: 1, rows: 1 },
+        { title: 'Card 7', cols: 1, rows: 1 },
+        { title: 'Card 8', cols: 1, rows: 1 }
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
