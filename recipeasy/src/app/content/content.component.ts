12363ce474aafea162bfd13815394777638a86b2
elistjames <ehstjames@gmail.com>
2022-03-15 01:23:15 -0600

    Made Content page extremely dynamic

--- recipeasy/src/app/content/content.component.ts
+++ recipeasy/src/app/content/content.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import {Observable} from "rxjs";
+import {from, Observable} from "rxjs";
+import {images} from './images'
 
 @Component({
   selector: 'app-content',
@@ -9,7 +10,7 @@ import {Observable} from "rxjs";
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-
+  public innerWidth: any;
   isHandset: boolean = false;
   /** Based on the screen size, switch from standard to one column per row */
   isHandsetObserver: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -22,9 +23,17 @@ export class ContentComponent {
   );
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
     this.isHandsetObserver.subscribe(currentObserverValue => {
       this.isHandset = currentObserverValue;
+      console.log(this.innerWidth);
     })
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = event.target.innerWidth;
+    console.log(this.innerWidth);
   }
 
   // colSize = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -41,23 +50,66 @@ export class ContentComponent {
       title: 'Pizza',
       ingredients: ['dough','sauce', 'toppings'],
       instructions : ['roll dough','put on sauce', 'put on toppings', 'cook'],
-      image: '../assets/post-images/image0.jpeg',
+      image: images[0],
       author: 'user44@email.com'
     },
     {
       title: 'Sauce',
       ingredients: ['tomato','pepper', 'oil'],
       instructions : ['cut up ingredients','blend'],
-      image: '../assets/post-images/image0.jpeg',
+      image: images[0],
       author: 'user20@email.com'
     },
     {
       title: 'Hambuger',
       ingredients: ['meat','buns', 'ketchup'],
-      instructions : ['smash meat','cook', 'put on toppings'],
-      image: '../assets/post-images/image0.jpeg',
+      instructions :   ['smash meat','cook', 'put on toppings'],
+      image: images[0],
       author: 'user49@email.com'
     },
+    {
+      title: 'Pizza',
+      ingredients: ['dough','sauce', 'toppings'],
+      instructions : ['roll dough','put on sauce', 'put on toppings', 'cook'],
+      image: images[0],
+      author: 'user44@email.com'
+    },
+    {
+      title: 'Sauce',
+      ingredients: ['tomato','pepper', 'oil'],
+      instructions : ['cut up ingredients','blend'],
+      image: images[0],
+      author: 'user20@email.com'
+    },
+    {
+      title: 'Hambuger',
+      ingredients: ['meat','buns', 'ketchup'],
+      instructions :   ['smash meat','cook', 'put on toppings'],
+      image: images[0],
+      author: 'user49@email.com'
+    },
+    {
+      title: 'Pizza',
+      ingredients: ['dough','sauce', 'toppings'],
+      instructions : ['roll dough','put on sauce', 'put on toppings', 'cook'],
+      image: images[0],
+      author: 'user44@email.com'
+    },
+    {
+      title: 'Sauce',
+      ingredients: ['tomato','pepper', 'oil'],
+      instructions : ['cut up ingredients','blend'],
+      image: images[0],
+      author: 'user20@email.com'
+    },
+    {
+      title: 'Hambuger',
+      ingredients: ['meat','buns', 'ketchup'],
+      instructions :   ['smash meat','cook', 'put on toppings'],
+      image: images[0],
+      author: 'user49@email.com'
+    },
+
   ];
 
   constructor(private breakpointObserver: BreakpointObserver) {}
