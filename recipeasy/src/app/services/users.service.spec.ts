9d37d105939622e3f546c9d82ae0e05191055616
TylerWitzke <twitzke6@outlook.com>
2022-03-29 13:18:15 -0600

    Final testing commit, 75% statement coverage

--- recipeasy/src/app/services/users.service.spec.ts
+++ recipeasy/src/app/services/users.service.spec.ts
@@ -1,5 +1,30 @@
 import { UsersService } from './users.service';
 import { TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from '../app-routing.module';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTabsModule} from "@angular/material/tabs";
+import { ImageCropperModule } from 'ngx-image-cropper';
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatListModule } from '@angular/material/list';
 
 
 
@@ -7,7 +32,35 @@ describe('RecipesService', () => {
   let service: UsersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        AppRoutingModule,
+        MatMenuModule,
+        MatIconModule,
+        MatGridListModule,
+        MatCardModule,
+        LayoutModule,
+        MatFormFieldModule,
+        MatCheckboxModule,
+        OverlayModule,
+        MatDialogModule,
+        MatInputModule,
+        FormsModule,
+        MatSelectModule,
+        MatRadioModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatTabsModule,
+        ImageCropperModule,
+        MatStepperModule,
+        MatListModule,
+        NoopAnimationsModule
+      ]
+    });
     service = TestBed.inject(UsersService);
   });
 
